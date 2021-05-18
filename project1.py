b1532af69befc0229c9bcabd919d2e23e2b7fa32
srinidhi-miyapuram <69413389+srinidhi-miyapuram@users.noreply.github.com>
2021-05-18 08:20:11 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -2,8 +2,9 @@ from googlesearch import search
 
 global links
 links = []
+search_name = input("Enter what you want to search here : ")
 
-result = search(query='buy cbd products online',tld='com',lang='en',num=20,stop=20,pause=2)
+result = search(query=search_name ,tld='com',lang='en',num=20,stop=20,pause=2)
 for link in result:
     links.append(link)
 print(links)
